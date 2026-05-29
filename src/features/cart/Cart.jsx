0efa193d0f38b874eca2c649d60d0f74cdba1648
2EZ4BLINK <blinkcode24@gmail.com>
2026-05-29 11:14:39 +0800

    update: reuse styles with apply and reusable components

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { LinkButton, Button } from "../../ui";
 import { Link } from "react-router-dom";
 
 const fakeCart = [
@@ -29,15 +30,13 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu" className="text-sm text-blue-500 hover:text-blue-600">
-        &larr; Back to menu
-      </Link>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Link to="/order/new">Order pizzas</Link>
-        <button>Clear cart</button>
+        <Button to="/order/new">Order pizzas</Button>
+        <Button>Clear cart</Button>
       </div>
     </div>
   );
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,14 +1,15 @@
-import { useNavigate, useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
+import LinkButton from "./LinkButton";
 
 const Error = () => {
-  const navigate = useNavigate();
   const { data, message } = useRouteError();
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
       <p>{data || message}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+
+      <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
   );
 };
